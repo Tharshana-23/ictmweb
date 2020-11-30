96e915d7d425d813cccb1a2329de8eb4dd037ef1
Tharshana-23 <sivatharshana@gmail.com>
2020-11-30 15:49:16 +0530

    first commit

---
+++ src/main/java/com/ictm/ictmweb/IctmwebApplication.java
@@ -0,0 +1,13 @@
+package com.ictm.ictmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IctmwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IctmwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ictm/ictmweb/IctmwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictm.ictmweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IctmwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
